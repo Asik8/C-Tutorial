7da5d9756dd5c7c4e06ca195b922a4d8375e2c99
Asik8 <motasembillah84@gmail.com>
2024-06-14 22:36:15 +0600

    codes added

---
+++ Class 1/DataTypePrint.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int a = 10;
+    // printf("data type", variableName);
+    printf("%d",a);
+    return 0;
+}
---
+++ Class 1/cBasic.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    // int --> 1 2 3 -7 -8 -9; %d decimal
+    // float --> 255.55678745; %f float
+    // double --> 255.321445456498545648; %lf long float
+    // char --> #@!$%^&*(); %c character
+    return 0;
+}
