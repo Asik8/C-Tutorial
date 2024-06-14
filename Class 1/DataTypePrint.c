00cefa3b9a1acc3c4657489f135688322958023f
Asik8 <motasembillah84@gmail.com>
2024-06-14 22:43:41 +0600

    class 1 done

--- Class 1/DataTypePrint.c
+++ Class 1/DataTypePrint.c
@@ -1,8 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int a = 10;
-    // printf("data type", variableName);
-    printf("%d",a);
+    // int a = 10;
+    // // printf("data type", variableName);
+    // printf("%d",a);
+
+    // float b = 10.1525545645;
+    // double c = 10.1525545645;
+    // printf("%f\n",b);
+    // printf("%lf",c);
+
+    char c = 'a';
+    printf("%c",c);
+
     return 0;
 }
